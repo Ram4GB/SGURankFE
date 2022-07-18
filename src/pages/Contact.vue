e1ee8d087e65ca3cab7145ad43e0f2f711e42535
Ram4GB <leminhcuong2988@yahoo.com>
2022-07-18 10:47:34 +0700

    edit contact us page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,6 +2,7 @@
   <div style="text-align: center">
     <h2>Liên hệ</h2>
     <p>Nếu như bạn có bất cứ câu hỏi nào hay muốn góp ý, bạn có thể gửi email cho tụi mình thông qua địa chỉ <a href="mailto:name@email.com">ram4gbsi@ram4gb.site</a></p>
+    <p>Hoặc đóng góp vào source <a href="https://github.com/Ram4GB/SGURankFE">https://github.com/Ram4GB/SGURankFE</a></p>
     <img :src="require('../assets/images/mail.gif')">
   </div>
 </template>
